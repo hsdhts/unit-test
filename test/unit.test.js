e2f974e5762d9429efc1ffb07f11f5a25bd84f58
husada20 <husadahutasoit@gmail.com>
2023-07-17 01:11:04 +0700

    unit test basic

---
+++ test/unit.test.js
@@ -0,0 +1,17 @@
+import {sum} from '../src/unit'
+
+test("sum function 1", () => {
+    const result = sum(1, 2)
+
+    expect (result).toBe(3);
+})
+test("sum function 2", () => {
+    const result = sum(1, 2)
+
+    expect (result).toBe(3);
+})
+test("sum function 3", () => {
+    const result = sum(1, 2)
+
+    expect (result).toBe(3);
+})
